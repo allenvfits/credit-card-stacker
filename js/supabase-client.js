0a1dc5b8b071838ab8c8a8b6b089fc1e1e808fd6
Allen <Hagotchaaa@gmail.com>
2026-04-25 05:56:38 -0700

    connect dashboard cards to supabase

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,5 +1,5 @@
 ﻿window.CREDIT_STACKER_SUPABASE_URL = "https://wsdhcosfuupowoavwgfp.supabase.co";
-window.CREDIT_STACKER_SUPABASE_ANON_KEY = "PeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndzZGhjb3NmdXVwb3dvYXZ3Z2ZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwOTM0NDUsImV4cCI6MjA5MjY2OTQ0NX0.ypjjgJZMwXEEj3oH0c1GykygFAWYXlVpT_AoiC8CsGIE";
+window.CREDIT_STACKER_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndzZGhjb3NmdXVwb3dvYXZ3Z2ZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwOTM0NDUsImV4cCI6MjA5MjY2OTQ0NX0.ypjjgJZMwXEEj3oH0c1GykygFAWYXlVpT_AoiC8CsGI";
 
 if (!window.supabaseClient) {
   window.supabaseClient = window.supabase.createClient(
